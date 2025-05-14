f8cdcc957bf7ed846e127d5128568b1bcc84b986
SamirCosta <samircfaduli@gmail.com>
2025-05-14 11:54:05 -0300

    Tela Fim Juntar ao Boss

---
+++ Projeto/Assets/3.Script/FloatingText.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatingText : MonoBehaviour
+{
+    [SerializeField] private GameObject btnMenu;
+    [SerializeField] private GameObject btnExit;
+    public float duration = 3f;         // Duração da animação
+
+    private RectTransform rectTransform;
+    private float elapsedTime = 0f;
+
+    void Start()
+    {
+        btnMenu.SetActive(false);
+        btnExit.SetActive(false);
+        rectTransform = GetComponent<RectTransform>();
+        StartCoroutine(MoveUp());
+    }
+
+    System.Collections.IEnumerator MoveUp()
+    {
+        Vector2 startPos = rectTransform.anchoredPosition;
+        Vector2 endPos = startPos + new Vector2(0, 1466);
+
+        while (elapsedTime < duration)
+        {
+            float t = elapsedTime / duration;
+            rectTransform.anchoredPosition = Vector2.Lerp(startPos, endPos, t);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        rectTransform.anchoredPosition = endPos;
+
+        yield return new WaitForSeconds(1);
+        btnMenu.SetActive(true);
+        btnExit.SetActive(true);
+    }
+}
